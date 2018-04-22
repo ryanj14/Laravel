516065ae1ccb246f59a72fcb31594a242e44ef8b
Ryan <datmanwich@gmail.com>
2018-04-22 14:53:17 -0700

    Added the create page

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,15 @@ Route::get('/whatever', function () {
     return "Who cares?";
 });
 
+Route::get('/places2', function () {
+    return view('pages/places2');
+});
+
 Route::get('contact', 'PagesController@contact');
 Route::get('about', 'PagesController@about');
 Route::get('posting/{id}', 'PostingsController@show');
 Route::resource('profile', 'CompanyProfileController');
 Route::resource('post', 'JobPostController');
 Route::get('users', 'UsersController@index');
+Route::get('users/{id}', 'UsersController@show')->name('showuser');
+Route::get('users/create', 'UsersController@create')->name('createuser');
